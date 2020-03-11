c3c2c7ea2937ebfa077ed74e7c782d20f307e3b9
joffitzer <joffitzer@gmail.com>
2020-03-11 11:57:22 -0400

    python for loops

---
+++ for-loop.py
@@ -0,0 +1,18 @@
+for letter in "Thing":
+    print(letter)
+
+friends = ["Jim", "Karen", "Kevin"]
+
+for friend in friends:
+    print(friend)
+
+for index in range(10):
+    print(index)
+
+
+for index in range(5):
+    if index == 0:
+        print("first index")
+    else:
+        print(index)
+
